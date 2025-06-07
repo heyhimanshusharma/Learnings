ceb019902b529b2c1a1e9648b6e9ef4790016176
Himanshu Sharrma <heyhimanshusharma@gmail.com>
2025-06-07 09:55:15 +0530

    Delete Python/Matplotlib directory

--- Python/Matplotlib/1.Scatter.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-X_data = np.random.random(500) * 100
-Y_data = np.random.random(500) * 100
-
-plt.scatter(X_data, Y_data, c="#00f", s=110, marker="*", alpha=0.5)
-plt.show()
--- Python/Matplotlib/2.line.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-years = [2006 + x for x in range(16)]
-weights = [80, 89, 43, 70, 59, 67, 89, 78, 78, 79, 76, 99, 88, 77, 78, 89 ]
-
-plt.plot(years, weights, c="g", lw=3, linestyle="--")
-plt.show()
