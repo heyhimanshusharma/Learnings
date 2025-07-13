0ec5c8f85869b77c159305e515cdbac5910d90f4
Himanshu Sharrma <heyhimanshusharma@gmail.com>
2025-07-13 23:05:51 +0530

    Add files via upload

---
+++ Python/Crash_Course_Python/16_Downloading_data/sitka_highs.py
@@ -0,0 +1,33 @@
+import csv
+import matplotlib.pyplot as plt
+
+filename = 'D:\Python\PCC\\16_Downloading_data\\data\\sitka_weather_07-2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    print(header_row)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
+    # Get high temperatures from this file
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+    
+    print(highs)
+
+    # Plot high temperatures.
+    plt.style.use('seaborn-v0_8')
+    fig, ax = plt.subplots()
+    ax.plot(highs, c='red')
+
+    # Format plot
+    plt.title("High temperatures, July 2018", fontsize=24)
+    plt.xlabel('', fontsize=16)
+    plt.ylabel("Temperature (F)", fontsize=16)
+    plt.tick_params(axis='both', which='major', labelsize=16)
+
+    plt.show()
+   
