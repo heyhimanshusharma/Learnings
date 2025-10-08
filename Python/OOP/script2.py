d3192606592ec2e6c304c46ef1edf775215a2e00
Himanshu Sharma <heyhimanshusharma@gmail.com>
2025-10-08 17:04:48 +0530

    some oops and html

--- Python/OOP/script2.py
+++ Python/OOP/script2.py
@@ -1,7 +1,8 @@
 class Dog:
-    def __init__(self, name, breed): # Init method for Data fields
+    def __init__(self, name, breed, owner): # Init method for Data fields
         self.name = name
         self.breed = breed
+        self.owner = owner
 
     def bark(self): # Method/func
         print("Woof woof")
@@ -9,14 +10,15 @@ class Dog:
 class Owner:
     def __init__(self, name, address, contact):
         self.name = name
-        
 
-dog1 = Dog("Bruce", "Scottish Terrier")
+owner1 = Owner("Himanshu", "Whitefield", "7721088212")
+dog1 = Dog("Bruce", "Scottish Terrier", owner1)
 dog1.bark()
 print(dog1.name)
 print(dog1.breed)
 
-dog2 = Dog("Tyson", "German Shepherd")
+owner2 = Owner("Louisa", "Manila", "8626089")
+dog2 = Dog("Tyson", "German Shepherd", owner2)
 dog2.bark()
 print(dog2.name)
 print(dog2.breed)
